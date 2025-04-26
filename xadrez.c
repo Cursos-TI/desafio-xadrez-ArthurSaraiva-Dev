b83be0ee26f99fdd3612ea2cdb428143f4b0901c
Arthur Saraiva <arthursaraiva.dev@gmail.com>
2025-04-26 02:03:00 +0000

    Desafio Nível MESTRE Concluído

--- xadrez.c
+++ xadrez.c
@@ -1,21 +1,73 @@
 #include <stdio.h>
 
+void movBispo(int bispo){
+    if(bispo <= 0){
+        return; // Definindo o caso base: quando a variável "bispo" chega a zero a recursão se encerra
+    }
+
+    for (int y = 0; y < 1; y++){ // Movimento vertical
+        printf("Cima\n");
+        for(int x = 0; x < 1; x++){ // Movimento Horizontal
+            printf("Direita\n");
+        }
+
+    }
+    movBispo(bispo - 1);
+    
+}
+
+void movTorre(int torre){
+    if(torre <= 0){
+        return; // Definindo o caso base: quando a variável "torre" chega a zero a recursão se encerra
+    }
+
+    printf("Direita\n");
+
+    movTorre(torre - 1);
+}
+
+void movRainha(int rainha){
+    if(rainha <= 0){
+        return; // Definindo o caso base: quando a variável "torre" chega a zero a recursão se encerra
+    }
+
+    printf("Esquerda\n");
+
+    movRainha(rainha - 1);
+}
+
+void movCavalo(int cavalo) {
+    
+    for (int mov = 1; mov <= cavalo; mov++) { // loop externo
+        for (int cima = 1; cima <= 2; cima++) { // loop interno
+            if (cima == 2 && mov % 2 == 0) { // condição para pular um movimento
+                printf("[Pulou!] "); // caso pule
+                continue; 
+            }
+            printf("Cima, "); //movimento
+        }
+        
+        for (int dir = 1; dir <= 1; dir++) { // lopp interno de movimento
+            printf("Direita"); // movimento
+        }
+        printf("\n"); // quebra de linha
+    }
+}
 int main() {
     // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-    int bispo = 1, torre = 1, rainha;
+    
+    // int bispo = 1, torre = 1, rainha;
+    
     // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-    printf("*** BISPO ***\n");
-    while (bispo <= 5){
+    /* printf("*** BISPO ***\n");
+        while (bispo <= 5){
         printf("Cima\n");
         printf("Direita\n");
         bispo++;
-    }
-    
+    */
+
     // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-    printf("*** TORRE ***\n");
+    /* printf("*** TORRE ***\n");
     do{
         if (torre <= 5)
         {
@@ -24,18 +76,20 @@ int main() {
         }
         
     } while (torre <= 5);
+    */
     
     // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    /*
     printf("*** RAINHA ***\n");
     // Lógica de movimentação:
     for (rainha = 1; rainha <= 8; rainha++){
         printf("Esquerda\n");
     }
+    */
+
     
     // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
+    /*
     printf("*** CAVALO ***\n");
     // Movimentação utilizando o for
     int movE = 1; // Controta a movimentação em L para a esquerda.
@@ -48,14 +102,24 @@ int main() {
         }
         printf("Esquerda\n"); // Retorna a movimentação para a esquerda
     }
-    
+    */
 
     // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
+    
+    //Bispo
+    printf("*** BISPO ***\n");
+    movBispo(5);
 
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+    //Torre
+    printf("*** TORRE ***\n");
+    movTorre(5);
 
+    //Rainha
+    printf("*** RAINHA ***\n");
+    movRainha(8);
+
+    //Cavalo
+    printf("*** CAVALO ***\n");
+    movCavalo(1);
     return 0;
 }
